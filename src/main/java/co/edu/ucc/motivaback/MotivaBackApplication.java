14eecb66d78f1a2f0cd798c4d41cb73a8055f2f3
Nestor Agredo <nagredo@progracol.com>
2021-11-30 08:02:23 -0500

    Initial commit

---
+++ src/main/java/co/edu/ucc/motivaback/MotivaBackApplication.java
@@ -0,0 +1,13 @@
+package co.edu.ucc.motivaback;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MotivaBackApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MotivaBackApplication.class, args);
+    }
+
+}
---
+++ src/test/java/co/edu/ucc/motivaback/MotivaBackApplicationTests.java
@@ -0,0 +1,13 @@
+package co.edu.ucc.motivaback;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MotivaBackApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
