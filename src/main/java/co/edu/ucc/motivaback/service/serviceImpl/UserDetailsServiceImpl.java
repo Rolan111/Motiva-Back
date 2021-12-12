336802bc9bcb320335abb203fbdfd5bc67758c8d
Nestor Agredo <nagredo@progracol.com>
2021-12-12 18:42:52 -0500

    Sprint2 HU1 -Improved SeederControl -Improved UserDetailsServiceImpl

--- src/main/java/co/edu/ucc/motivaback/service/serviceImpl/UserDetailsServiceImpl.java
+++ src/main/java/co/edu/ucc/motivaback/service/serviceImpl/UserDetailsServiceImpl.java
@@ -1,7 +1,10 @@
 package co.edu.ucc.motivaback.service.serviceImpl;
 
 import co.edu.ucc.motivaback.dto.UserDto;
-import com.google.cloud.firestore.DocumentReference;
+import com.google.api.core.ApiFuture;
+import com.google.cloud.firestore.CollectionReference;
+import com.google.cloud.firestore.DocumentSnapshot;
+import com.google.cloud.firestore.QuerySnapshot;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
@@ -17,8 +20,25 @@ public class UserDetailsServiceImpl implements UserDetailsService {
 
     @Override
     public UserDetails loadUserByUsername(String email) throws UsernameNotFoundException {
-        DocumentReference user = this.firebase.getFirestore().collection("user").document(email);
+        UserDto userDto = new UserDto();
 
-        return new UserDto();
+        ApiFuture<QuerySnapshot> querySnapshotApiFuture = getCollection().get();
+        try {
+            for (DocumentSnapshot doc : querySnapshotApiFuture.get().getDocuments()) {
+                if (doc.toObject(UserDto.class).getEmail().equals(email)) {
+                    userDto = doc.toObject(UserDto.class);
+                    userDto.setId(doc.getId());
+                }
+            }
+
+            return userDto;
+
+        } catch (Exception e) {
+            return null;
+        }
+    }
+
+    private CollectionReference getCollection() {
+        return firebase.getFirestore().collection("user");
     }
 }
