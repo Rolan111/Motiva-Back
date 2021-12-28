05dce6d6b9763f500d709f390e4de78889ce4265
Nestor Agredo <nagredo@progracol.com>
2021-12-27 21:57:42 -0500

    Sprint2 HU1 -Add SeederControl -Add rolEnum

---
+++ src/main/java/co/edu/ucc/motivaback/config/security/SeederControl.java
@@ -0,0 +1,76 @@
+package co.edu.ucc.motivaback.config.security;
+
+import co.edu.ucc.motivaback.dto.UserDto;
+import co.edu.ucc.motivaback.enums.RegisterStatusEnum;
+import co.edu.ucc.motivaback.enums.UserRolEnum;
+import co.edu.ucc.motivaback.service.impl.FirebaseInitializer;
+import com.google.cloud.firestore.CollectionReference;
+import org.springframework.context.event.ContextRefreshedEvent;
+import org.springframework.context.event.EventListener;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.stereotype.Component;
+
+import java.util.Date;
+import java.util.HashMap;
+import java.util.Map;
+
+@Component
+public class SeederControl {
+    private BCryptPasswordEncoder bCryptPasswordEncoder;
+    private final FirebaseInitializer firebase;
+
+    public SeederControl(BCryptPasswordEncoder bCryptPasswordEncoder, FirebaseInitializer firebase) {
+        this.bCryptPasswordEncoder = bCryptPasswordEncoder;
+        this.firebase = firebase;
+        SecurityContextHolder.getContext().setAuthentication(new UsernamePasswordAuthenticationToken(
+                new AuthenticatedUser(1L, "a@a.com", ""),
+                "key"
+        ));
+    }
+
+    @EventListener
+    public void seed(ContextRefreshedEvent contextRefreshedEvent) {
+        this.userSeed();
+    }
+
+    protected void userSeed() {
+        UserDto userDto = new UserDto();
+        userDto.setEmail("a@b.com");
+        userDto.setFullname("Test test");
+        userDto.setIdentification("123456789");
+        userDto.setUserRol(UserRolEnum.COORD_GENERAL);
+        System.out.println("***************************************************");
+        String encode = bCryptPasswordEncoder.encode("motivapw");
+        System.out.println(encode);
+        userDto.setPassword(encode);
+        userDto.setPhone("456456");
+        userDto.setCreatedAt(new Date());
+        userDto.setCreatedBy(1L);
+        userDto.setStatus(RegisterStatusEnum.ACTIVE);
+
+        Map<String, Object> docData = getDocData(userDto);
+
+//        ApiFuture<WriteResult> writeResultApiFuture = getCollection().document().create(docData);
+//        System.out.println(writeResultApiFuture);
+    }
+
+    private CollectionReference getCollection() {
+        return firebase.getFirestore().collection("user");
+    }
+
+    private Map<String, Object> getDocData(UserDto userDto) {
+        Map<String, Object> docData = new HashMap<>();
+        docData.put("email", userDto.getEmail());
+        docData.put("fullname", userDto.getFullname());
+        docData.put("identification", userDto.getIdentification());
+        docData.put("userRol", userDto.getUserRol());
+        docData.put("password", userDto.getPassword());
+        docData.put("phone", userDto.getPhone());
+        docData.put("createAt", userDto.getCreatedAt());
+        docData.put("createBy", userDto.getCreatedBy());
+        docData.put("status", userDto.getStatus());
+        return docData;
+    }
+}
