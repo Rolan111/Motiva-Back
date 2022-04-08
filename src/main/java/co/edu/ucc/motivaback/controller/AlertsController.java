16d0e7a31482fa94a42ca3b362b0a9f08fd3c38b
Rolan <estibenruiz05@gmail.com>
2022-04-08 14:55:42 -0500

    Improve code

--- src/main/java/co/edu/ucc/motivaback/controller/AlertsController.java
+++ src/main/java/co/edu/ucc/motivaback/controller/AlertsController.java
@@ -1,6 +1,6 @@
 package co.edu.ucc.motivaback.controller;
 
-import co.edu.ucc.motivaback.entity.AlertsEntity;
+import co.edu.ucc.motivaback.entity.AlertEntity;
 import com.google.api.core.ApiFuture;
 import com.google.cloud.firestore.*;
 import com.google.firebase.cloud.FirestoreClient;
@@ -14,27 +14,39 @@ import java.util.concurrent.ExecutionException;
 @RequestMapping("/api")
 public class AlertsController {
 
-    @GetMapping(value = "/alerts")
-    public List<AlertsEntity> alerts() throws ExecutionException, InterruptedException {
-        List<AlertsEntity> commentsEntities = new ArrayList<>();
+    int tamanioLista;
+
+    @GetMapping(value = "/alerts_size") //Consultamos la cantidad de registros
+    public int alertsSize() throws ExecutionException, InterruptedException {
+        Firestore db = FirestoreClient.getFirestore();
+        CollectionReference documentReference = db.collection("alert_pruebas");
+        ApiFuture<QuerySnapshot> future = documentReference.get();
+        List<QueryDocumentSnapshot> documents = future.get().getDocuments();
+        System.out.println("El tamanio del array es: "+documents.size());
+        tamanioLista = documents.size();
+        return tamanioLista;
+    }
+
+    @GetMapping(value = "/alert")
+    public List<AlertEntity> alerts() throws ExecutionException, InterruptedException {
+        List<AlertEntity> commentsEntities = new ArrayList<>();
 
         Firestore db = FirestoreClient.getFirestore();
-        CollectionReference documentReference = db.collection("alerts");
+        CollectionReference documentReference = db.collection("alert");
         ApiFuture<QuerySnapshot> future = documentReference.get();
         List<QueryDocumentSnapshot> documents = future.get().getDocuments();
 
         for (QueryDocumentSnapshot doc : documents){
-            AlertsEntity commentsDto = doc.toObject(AlertsEntity.class);
+            AlertEntity commentsDto = doc.toObject(AlertEntity.class);
             commentsEntities.add(commentsDto);
         }
         return commentsEntities;
     }
 
-    @PostMapping(value = "/alerts-create")
-    public String saveAlerts(@RequestBody AlertsEntity alertsEntity) throws ExecutionException, InterruptedException {
+    @PostMapping(value = "/alert-create")
+    public String saveAlerts(@RequestBody AlertEntity alertsEntity) throws ExecutionException, InterruptedException {
         Firestore db = FirestoreClient.getFirestore();
-        db.collection("alerts").document().set(alertsEntity);
+        db.collection("alert").document().set(alertsEntity);
         return "Hola mundo";
-
     }
 }
