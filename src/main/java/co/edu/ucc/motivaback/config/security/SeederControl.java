c230be115c5fa5ba99055bfb15fdf3cd84962b70
oscardacto <oscardacto@gmail.com>
2022-09-29 11:31:32 -0500

    Mejora Creación y autorización de usuarios (P_Campo)

--- src/main/java/co/edu/ucc/motivaback/config/security/SeederControl.java
+++ src/main/java/co/edu/ucc/motivaback/config/security/SeederControl.java
@@ -1,3 +1,4 @@
+/*
 package co.edu.ucc.motivaback.config.security;
 
 import co.edu.ucc.motivaback.dto.UserDto;
@@ -20,10 +21,15 @@ import java.util.Date;
 import java.util.HashMap;
 import java.util.Map;
 
+import static co.edu.ucc.motivaback.enums.UserRolEnum.P_CAMPO;
+import static co.edu.ucc.motivaback.enums.UserRolEnum.SUPERVISOR;
+
 @Component
 public class SeederControl {
     private final BCryptPasswordEncoder bCryptPasswordEncoder;
     private final FirebaseInitializer firebase;
+
+
 
     public SeederControl(BCryptPasswordEncoder bCryptPasswordEncoder, FirebaseInitializer firebase) {
         this.bCryptPasswordEncoder = bCryptPasswordEncoder;
@@ -41,42 +47,14 @@ public class SeederControl {
 
     protected void userSeed() {
 
-        Map<String, Object> docData1 = getDocData(addUser("Chanchi", "Kelly Daniela", "1061801108", "Ck1061801108"));
-        Map<String, Object> docData2 = getDocData(addUser("Lucio Esparza", "Joselyne Viviana", "1061820133", "Lj1061820133"));
-        Map<String, Object> docData3 = getDocData(addUser("Gómez Jimenez", "Karol Lizeth", "1062754529", "Gk1062754529"));
-        Map<String, Object> docData4 = getDocData(addUser("Alomia Bravo", "Andrés Julian", "1061776800", "Aa1061776800"));
-        Map<String, Object> docData5 = getDocData(addUser("Camargo Restrepo", "Angie Marcela", "1060877207", "Ca1060877207"));
-        Map<String, Object> docData6 = getDocData(addUser("Hurtado Medina", "María Fernanda", "25277453", "Hm25277453"));
-        Map<String, Object> docData7 = getDocData(addUser("Giraldo Diaz", "Diana Karina", "31714967", "Gd31714967"));
-        Map<String, Object> docData8 = getDocData(addUser("Hoyos Quintero", "Julieth Carolina", "1061800130", "Hj1061800130"));
-        Map<String, Object> docData9 = getDocData(addUser("Córdoba Llanten", "Angela Marcela", "1061754099", "Ca1061754099"));
-        Map<String, Object> docData10 = getDocData(addUser("Guamanga", "Adriana Lucia", "1059356316", "Ga1059356316"));
-        Map<String, Object> docData11 = getDocData(addUser("Ramirez Valencia", "Jhon Jairo", "76327915", "Rj76327915"));
-        Map<String, Object> docData12 = getDocData(addUser("Orozco Zuñiga", "Aura Maria", "1061790557", "Oa1061790557"));
-        Map<String, Object> docData13 = getDocData(addUser("Cumbalaza Moncada", "Dianne Magyeli", "1061773045", "Md1061773045"));
-        Map<String, Object> docData14 = getDocData(addUser("Gomez Flor", "Jennifer Natalia", "1002972339", "Gj1002972339"));
-        Map<String, Object> docData15 = getDocData(addUser("Omen Quinayas", "Deiby Socorro", "25485963", "Od25485963"));
-        Map<String, Object> docData16 = getDocData(addUser("Muñoz Martinez", "Jessica Fernanda", "1062317907", "Mj1062317907"));
-        Map<String, Object> docData17 = getDocData(addUser("Guerrero Valencia", "Karen Jhuliana", "1061600768", "Gk1061600768"));
+        Map<String, Object> docData1 = getDocData(addUserPCampo(1L,"Chanchi", "Kelly Daniela", "1061801108", "Ck1061801108", P_CAMPO));
+        Map<String, Object> docData2 = getDocData(addUserPCampo(2L,"Campo", "Daniel", "1020604689", "Cd1020604689", SUPERVISOR));
+
 
         ArrayList<Map<String, Object>> listDocData = new ArrayList<>();
         listDocData.add(docData1);
         listDocData.add(docData2);
-        listDocData.add(docData3);
-        listDocData.add(docData4);
-        listDocData.add(docData5);
-        listDocData.add(docData6);
-        listDocData.add(docData7);
-        listDocData.add(docData8);
-        listDocData.add(docData9);
-        listDocData.add(docData10);
-        listDocData.add(docData11);
-        listDocData.add(docData12);
-        listDocData.add(docData13);
-        listDocData.add(docData14);
-        listDocData.add(docData15);
-        listDocData.add(docData16);
-        listDocData.add(docData17);
+
 
         for (Map<String, Object> docDataI : listDocData) {
             ApiFuture<WriteResult> writeResultApiFuture = getCollection().document().create(docDataI);
@@ -84,12 +62,13 @@ public class SeederControl {
         }
     }
 
-    private UserDto addUser(String lastName, String name, String identification, String password) {
+    private UserDto addUserPCampo(Long idUser,String lastName, String name, String identification, String password, UserRolEnum jobProfile) {
         UserDto userDto = new UserDto();
+        userDto.setIdUser(idUser);
         userDto.setLastName(lastName);
         userDto.setName(name);
         userDto.setIdentification(identification);
-        userDto.setJobProfile(UserRolEnum.P_CAMPO);
+        userDto.setJobProfile(jobProfile);
         System.out.println("*****************");
         String encode = bCryptPasswordEncoder.encode(password);
         System.out.println(encode);
@@ -108,6 +87,7 @@ public class SeederControl {
     private Map<String, Object> getDocData(UserDto userDto) {
         Map<String, Object> docData = new HashMap<>();
         docData.put(CommonsService.LAST_NAME, userDto.getLastName());
+        docData.put("id_user", userDto.getIdUser());
         docData.put("name", userDto.getName());
         docData.put("identification", userDto.getIdentification());
         docData.put("job_profile", userDto.getJobProfile());
@@ -119,3 +99,4 @@ public class SeederControl {
         return docData;
     }
 }
+*/
