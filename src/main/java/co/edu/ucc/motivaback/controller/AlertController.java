a7a7de88217c7aac7ee6e7dbb15bd4203546bfd0
Rolan <estibenruiz05@gmail.com>
2022-11-14 14:28:57 -0500

    improve alert

--- src/main/java/co/edu/ucc/motivaback/controller/AlertController.java
+++ src/main/java/co/edu/ucc/motivaback/controller/AlertController.java
@@ -31,9 +31,44 @@ import static co.edu.ucc.motivaback.util.CommonsService.CREATED_OK;
 public class AlertController {
 
     private final AlertService alertEntity;
+    int tamanioLista;
 
     public AlertController(AlertService alertEntity) {
         this.alertEntity = alertEntity;
+    }
+
+    @GetMapping(value = "/alerts_size") //Consultamos la cantidad de registros
+    public int alertsSize() throws ExecutionException, InterruptedException {
+        Firestore db = FirestoreClient.getFirestore();
+        CollectionReference documentReference = db.collection("alert_pruebas");
+        ApiFuture<QuerySnapshot> future = documentReference.get();
+        List<QueryDocumentSnapshot> documents = future.get().getDocuments();
+        System.out.println("El tamanio del array es: "+documents.size());
+        tamanioLista = documents.size();
+        return tamanioLista;
+    }
+
+    @GetMapping(value = "/alert")
+    public List<AlertEntity> alerts() throws ExecutionException, InterruptedException {
+        List<AlertEntity> commentsEntities = new ArrayList<>();
+
+        Firestore db = FirestoreClient.getFirestore();
+        CollectionReference documentReference = db.collection("alert");
+        ApiFuture<QuerySnapshot> future = documentReference.get();
+        List<QueryDocumentSnapshot> documents = future.get().getDocuments();
+
+        for (QueryDocumentSnapshot doc : documents){
+            AlertEntity commentsDto = doc.toObject(AlertEntity.class);
+            commentsEntities.add(commentsDto);
+        }
+        return commentsEntities;
+    }
+
+    @PostMapping(value = "/alert-create")
+    public String saveAlerts(@RequestBody AlertEntity alertsEntity) throws ExecutionException, InterruptedException {
+        Firestore db = FirestoreClient.getFirestore();
+        db.collection("alert").document().set(alertsEntity);
+        return "Hola mundo";
     }
 
     @GetMapping(value = "/alerts")
