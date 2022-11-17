210c45210b93af1e835409f87863926e85a70a57
Rolan <estibenruiz05@gmail.com>
2022-11-17 11:02:48 -0500

    Configurando master 2

--- src/main/java/co/edu/ucc/motivaback/controller/AlertController.java
+++ src/main/java/co/edu/ucc/motivaback/controller/AlertController.java
@@ -35,10 +35,10 @@ public class AlertController {
         this.alertEntity = alertEntity;
     }
 
-    //cambios nuevos
     int tamanioLista;
 
-    //Persistir commit, en master también 2
+    //Persistir commit
+    //Cambio nuevo desde alert DEV
 
     @GetMapping(value = "/alertByIdPoll/{idPoll}")
     public List<AlertEntity> alertByIdPoll(@PathVariable String idPoll) throws ExecutionException, InterruptedException {
